884a37c2aac33ca1c1f500f7b15de967bd75fe98
pengyaohui <yaohui.peng@coollu.com.cn>
2019-08-27 16:09:52 +0800

    123

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'assets',
     assetsPublicPath: '/',
     proxyTable: {},
 
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/yihanWebD/',
 
     /**
      * Source Maps
